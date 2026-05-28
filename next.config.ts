1cfffecc3ece97207f9cd5dc04ba8b32e32207fb
Sneha Soni <baberwalsnehaa@gmail.com>
2026-05-28 14:04:38 +0530

    changed key name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
   },
 };
 
